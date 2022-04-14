e43b3c94bd503b45e731a08dbaf4654e87bf3e6a
Onur Demiralay <onur@agilitycorp.com>
2022-04-13 17:52:51 -0700

    Initial Commit

---
+++ EasingInterpolator/EasingInterpolator.h
@@ -0,0 +1,18 @@
+//
+//  EasingInterpolator.h
+//  EasingInterpolator
+//
+//  Created by Onur Demiralay on 2022-04-13.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EasingInterpolator.
+FOUNDATION_EXPORT double EasingInterpolatorVersionNumber;
+
+//! Project version string for EasingInterpolator.
+FOUNDATION_EXPORT const unsigned char EasingInterpolatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EasingInterpolator/PublicHeader.h>
+
+
